688f5b508460f9b8794cfb1a2c32c00ef908221e
Rodolfo Tiburcio <fulladdict@gmail.com>
2022-04-30 13:38:01 -0500

    comments added

--- app/forms.py
+++ app/forms.py
@@ -3,5 +3,8 @@ from flask_wtf.file import FileField, FileAllowed
 from wtforms.fields.simple import MultipleFileField, SubmitField
 
 class UploadFiles(FlaskForm):
+    '''
+    Form to unpload xml invoice files
+    '''
     files = MultipleFileField('Archivos XML', validators=[FileAllowed(['xml'], 'Solo archivos XML')])
     submit = SubmitField('Subir archivos')
